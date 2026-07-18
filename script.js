d634545a44fa9c81bd6e5c703bd7978e51ca216c
Aman Chapadiya <chapadiya.aman@gmail.com>
2026-07-18 21:11:26 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js
+// Currently no interactive javascript logic is required based on the provided design.
+// The "View Details" buttons are implemented as anchor tags with hrefs.
+console.log("Engineering Internships page loaded successfully.");
